47811b180a2a482d9dc5c58fe0c203959995d64f
dahea67980 <dahea67980@gmail.com>
2026-04-28 18:02:56 +0900

    자바 작업

--- js/test.js
+++ js/test.js
@@ -1,5 +1,25 @@
-let num = 10;
+// // 키보드 입력 방식을 반드시 알아야 한다.
+// // prompt()
 
-if (num % 2 === 0) {
-  console.log("num은 짝수이다!!");
+// // let input = parseInt(prompt("정수 입력하시오."));
+
+// while (1) {
+//   let input = parseInt(prompt("정수 입력하시오."));
+//   if (input === 7) {
+//     console.log("7이 입력되었으니 종료합니다.");
+//     break;
+//   }
+// }
+
+while (1) {
+  let input = parseInt(prompt("정수 입력하시오."));
+  if (input === 7) {
+    console.log("7이 입력되었으니 종료합니다.");
+    break;
+  }
+  let tot = 0;
+  for (i = 0; i <= input; i++) {
+    tot = tot + i;
+  }
+  console.log(tot);
 }
